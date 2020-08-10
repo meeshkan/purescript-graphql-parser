1ec404d1ce8dddba25e9e7bccba23c5434a03a69
Mike Solomon <mike@meeshkan.com>
2020-08-10 19:35:24 +0300

    Adds additional check in interface test

--- test/Data/GraphQL/ParseFull3.purs
+++ test/Data/GraphQL/ParseFull3.purs
@@ -3,12 +3,17 @@ module Test.Data.GraphQL.ParseFull3 where
 import Prelude
 import Data.Either (either)
 import Data.GraphQL.AST as AST
+import Data.GraphQL.Parser (implementsInterfaces)
 import Data.GraphQL.Parser as GP
-import Data.Lens (class Wander)
-import Data.Lens as L
-import Data.List (length)
+import Data.Lens (class Wander, Prism', _2, _Just, preview, prism', toListOf, traversed)
+import Data.Lens.Common (simple)
+import Data.Lens.Iso.Newtype (_Newtype)
+import Data.Lens.Record (prop)
+import Data.List (List(..), length, (:))
+import Data.Maybe (Maybe(..))
 import Data.Profunctor.Choice (class Choice)
-import Data.Tuple (uncurry)
+import Data.Symbol (SProxy(..))
+import Data.Tuple (Tuple(..), uncurry)
 import Effect.Aff (Aff)
 import Effect.Class (liftEffect)
 import Effect.Exception (throw)
@@ -25,12 +30,20 @@ parseDocument t = do
 
 lensToObjectDefinitions ∷ ∀ m. Choice m ⇒ Wander m ⇒ m AST.ObjectTypeDefinition AST.ObjectTypeDefinition → m AST.Document AST.Document
 lensToObjectDefinitions =
-  ( uncurry L.prism' AST._Document
-      <<< L.traversed
-      <<< uncurry L.prism' AST._Definition_TypeSystemDefinition
-      <<< uncurry L.prism' AST._TypeSystemDefinition_TypeDefinition
-      <<< uncurry L.prism' AST._TypeDefinition_ObjectTypeDefinition
+  ( uncurry prism' AST._Document
+      <<< traversed
+      <<< uncurry prism' AST._Definition_TypeSystemDefinition
+      <<< uncurry prism' AST._TypeSystemDefinition_TypeDefinition
+      <<< uncurry prism' AST._TypeDefinition_ObjectTypeDefinition
   )
+
+peel :: forall a. Prism' (List a) (Tuple a (List a))
+peel =
+  prism' (\(Tuple f s) -> (Cons f s))
+    ( \l -> case l of
+        Nil -> Nothing
+        (Cons a b) -> Just $ Tuple a b
+    )
 
 testSwapi ∷ ∀ m. Monad m ⇒ SpecT Aff Unit m Unit
 testSwapi =
@@ -39,4 +52,20 @@ testSwapi =
       $ do
           -- 52 was confirmed by a quick n' dirty parsing of the document in python
           it "should get 52 type definitions" \doc → do
-            (length (L.toListOf lensToObjectDefinitions doc)) `shouldEqual` 52
+            (length (toListOf lensToObjectDefinitions doc)) `shouldEqual` 52
+          it "should hvae a type Film that implements an interface called Node" \doc → do
+            preview
+              ( simple _Newtype
+                  <<< peel
+                  <<< _2
+                  <<< traversed
+                  <<< uncurry prism' AST._Definition_TypeSystemDefinition
+                  <<< uncurry prism' AST._TypeSystemDefinition_TypeDefinition
+                  <<< uncurry prism' AST._TypeDefinition_ObjectTypeDefinition
+                  <<< simple _Newtype
+                  <<< (prop (SProxy :: SProxy "implementsInterfaces"))
+                  <<< _Just
+              )
+              doc
+              `shouldEqual`
+                Just (AST.ImplementsInterfaces (AST.NamedType "Node" : Nil))
